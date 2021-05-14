71773bb32c43dadc155d772c5b3cf282ac27635a
RashiGoenka <82649591+RashiGoenka@users.noreply.github.com>
2021-05-14 14:45:17 +0530

    Update VehicleDAOInt.java

--- AppartmentSecurity/src/main/java/com/cg/aps/repository/VehicleDAOInt.java
+++ AppartmentSecurity/src/main/java/com/cg/aps/repository/VehicleDAOInt.java
@@ -8,22 +8,18 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.cg.aps.entities.VehicleEntity;
-
+/**
+* @author Rashi Goenka
+*            
+*/
 
 @Repository
 public interface VehicleDAOInt extends JpaRepository<VehicleEntity,String>{
 
-//	public long add(VehicleEntity bean);
-//	
-//	public void update(VehicleEntity bean);
-//	
-//	public void delete(VehicleEntity bean);
+
 
 	public List<VehicleEntity> findByName(String name);
-//	
 
-//	
-//	public List<VehicleEntity> search(VehicleEntity bean);
 	
 	
 }
