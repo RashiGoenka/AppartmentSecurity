1f3ea68f8593c84dbd299d6c053dcabbf01f483a
ojhaashutosh <81087436+ojhaashutosh@users.noreply.github.com>
2021-05-17 21:14:01 +0530

    Add files via upload

---
+++ src/main/java/com/cg/aps/service/VisitorServiceImpl.java
@@ -0,0 +1,65 @@
+package com.cg.aps.service;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cg.aps.entities.VisitorEntity;
+import com.cg.aps.exception.RecordNotFoundException;
+import com.cg.aps.repository.VisitorDAOInt;
+@Service
+@Transactional
+public class VisitorServiceImpl implements VisitorServiceInt
+{
+	@Autowired
+	VisitorDAOInt dao;
+
+	@Override
+	public VisitorEntity add(VisitorEntity bean) {
+		
+		System.out.println("Visitor added");
+		return dao.save(bean);
+	}
+
+	@Override
+	public VisitorEntity  update(VisitorEntity bean) {
+		return dao.save(bean);
+	}
+
+	@Override
+	public void delete(VisitorEntity bean) {
+		dao.deleteById(bean.getFlatNo());
+	}
+
+	@Override
+	public List<VisitorEntity> findByName(String name) throws RecordNotFoundException
+	{
+		List<VisitorEntity> list= dao.findByName(name);
+		if(list.isEmpty())
+		{
+			throw new RecordNotFoundException();
+			
+		}
+		
+	return list;
+		
+	}
+
+	
+
+	@Override
+	public List<VisitorEntity> search() throws RecordNotFoundException
+	{
+		List<VisitorEntity> list= dao.findAll();
+		if(list.isEmpty())
+		{
+			throw new RecordNotFoundException();
+			
+		}
+		return list;
+	}
+
+}
